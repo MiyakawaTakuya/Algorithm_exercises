5b146e142179ff2cd50ec7bb28fc64a30a3fa778
Miyakawa Takuya <omyakawa@gmail.com>
2022-04-09 02:05:10 +0900

    refactored MaximumSubarray.py

--- python/LeetCode/MaximumSubarray.py
+++ python/LeetCode/MaximumSubarray.py
@@ -5,7 +5,6 @@ from typing import List
 # 連続した部分配列（少なくとも1つの数値を含む）の総和の最大値を見つけ、その総和を返しなさい。
 
 
-# WIP
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         length = len(nums)
@@ -33,16 +32,48 @@ class Solution:
         return max(max(nums), maxCenter)
 
 
+# Approach 1: Optimized Brute Force   →Time Limit Exceeded
+# Algorithm
+# Initialize a variable maxSubarray = -infinity to keep track of the best subarray.
+# We need to use negative infinity, not 0, because it is possible that there are only negative numbers in the array.
+# Use a for loop that considers each index of the array as a starting point.
+# For each starting point, create a variable currentSubarray = 0. Then, loop through the array from the starting index,
+# adding each element to currentSubarray. Every time we add an element it represents a possible subarray
+# - so continuously update maxSubarray to contain the maximum out of the currentSubarray and itself.
+# Return maxSubarray.
+
+
+    def maxSubArrayA(self, nums: List[int]) -> int:
+        max_subarray = -float('inf')   # -math.inf → error
+        for i in range(len(nums)):
+            current_subarray = 0
+            for j in range(i, len(nums)):
+                current_subarray += nums[j]
+                max_subarray = max(max_subarray, current_subarray)
+        return max_subarray
+
+# Approach 2: Dynamic Programming, Kadane's Algorithm
+    def maxSubArrayA2(self, nums: List[int]) -> int:
+        # Initialize our variables using the first element.
+        current_subarray = max_subarray = nums[0]
+        # Start with the 2nd element since we already used the first one.
+        for num in nums[1:]:
+            # If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            current_subarray = max(num, current_subarray + num)
+            max_subarray = max(max_subarray, current_subarray)
+        return max_subarray
+
+
 solution = Solution()
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 # Output: 6
 # Explanation: [4, -1, 2, 1] has the largest sum = 6.
-print(solution.maxSubArray(nums))
+print(solution.maxSubArrayA2(nums))
 nums = [1]
-print(solution.maxSubArray(nums))
+print(solution.maxSubArrayA2(nums))
 nums = [5, 4, -1, 7, 8]
-print(solution.maxSubArray(nums))
+print(solution.maxSubArrayA2(nums))
 nums = [-2, -1]  # -1
-print(solution.maxSubArray(nums))
-[0, -3, 1, 1]  # 2
-print(solution.maxSubArray(nums))
+print(solution.maxSubArrayA2(nums))
+nums = [0, -3, 1, 1]  # 2
+print(solution.maxSubArrayA2(nums))
